4d404f6ec4d48f9a53827b932e4612e77a4e6e29
Paul <pauljohnbonganciso030@gmail.com>
2026-03-04 21:42:35 +0800

    Add files via upload

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,6 @@
 <template>
-		
+	
+
 	<div class="col-8 col-sm-5 col-md-3 col-lg-2">
 	<a href="#" id="scrollTopBtn" class="btn btn-secondary ">
 	    ↑
@@ -7,7 +8,7 @@
 	</div>
 
 	<footer class="mt-5 p-3 text-light">
-	    <h6 class="text-center">Charles Babbage</h6>
+	    <h6 class="text-center">Paul John Bonganciso</h6>
 	    <p class="text-center mb-1">Full Stack Web Developer</p>
 	    <p class="text-center">&copy; All Rights Reserved 2025</p>
 	</footer>
